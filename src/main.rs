65f9b3f955f6186ca1248d9eb7cf18d3b2e74a8d
Lucxx2845 <lucyazure2845@gmail.com>
2023-06-16 10:26:31 +0100

    Implement mainmenu loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::process::Command;
+use std::{process::Command, str::FromStr};
 
 fn main() {
     println!("Hello, world!");
+    let choice = mainmenu();
 }
 
 enum Choice {
@@ -23,18 +24,32 @@ impl std::str::FromStr for Choice {
             3 => Ok(Self::AddEmu),
             4 => Ok(Self::ReadGameDatabase),
             5 => Ok(Self::ReadEmuDatabase),
-            _ => Err(anyhow::anyhow!("Invalid choice")),
+            _ => Err(anyhow::anyhow!("Invalid variant")),
         }
     }
 }
 
-fn mainmenu() {
-    println!(
-        "Choose an action:
+fn input() -> String {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s
+}
+
+fn mainmenu() -> Choice {
+    loop {
+        println!(
+            "Choose an action:
     (1) Start a game
     (2) Add a game
     (3) Add an emulator
     (4) Read the games database
     (5) Read the emulators database"
-    );
+        );
+        let choice_str = input();
+        let choice = Choice::from_str(&choice_str);
+        match choice {
+            Ok(c) => break c,
+            Err(_) => panic!("Fuck"),
+        }
+    }
 }
